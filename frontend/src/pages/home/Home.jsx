e9674f36fa0bb880a6584457fc6fec78bffab7ad
Dennis Peter <peterdennis573@gmail.com>
2024-09-20 09:37:42 +0300

    The project instanciated and components worked upon

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import Sidebar from "../../components/sidebar/Sidebar";
+import MessageContainer from "../../components/messages/MessageContainer";
+
+const Home = () => {
+  return (
+    <div
+      className="home-container"
+      
+    >
+      <div className="rounded-lg overflow-hidden  bg-clip-padding backdrop-filter backdrop-blur-lg  container home-wrapper">
+        <Sidebar />
+        <MessageContainer />
+      </div>
+    </div>
+  );
+};
+export default Home;
